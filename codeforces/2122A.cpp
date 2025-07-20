a3e8cb10b6a9a74ac922efdf2825a620b7579281
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-07-20 11:34:09 +0600

    add codeforces solution for 2122A

---
+++ codeforces/2122A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<ll, ll> pi;
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    if( n== 1 || m == 1 || (n==2 && m == 2)){
+        no;
+        return;
+    }
+    yes;
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
